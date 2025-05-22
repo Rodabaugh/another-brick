1a5d68af9b5b3e98944835928b617b14a0e73ed6
Erik Rodabaugh <erik@erikrodabaugh.com>
2025-05-21 17:31:41 -0700

    SQLC

---
+++ sql/queries/posts.sql
@@ -0,0 +1,12 @@
+-- name: CreatePost :one
+INSERT INTO posts (id, created_at, updated_at, content)
+VALUES (
+    gen_random_uuid(), NOW(), NOW(), $1
+)
+RETURNING *;
+
+-- name: GetPosts :many
+SELECT * FROM posts;
+
+-- name: GetPostByID :one
+SELECT * FROM posts WHERE id = $1;
