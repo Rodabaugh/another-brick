a2427ab29da65613e6b251170376a0ab69239b43
Erik Rodabaugh <erik@erikrodabaugh.com>
2025-05-21 17:11:28 -0700

    Init commit

---
+++ design.md
@@ -0,0 +1,24 @@
+**Backend**
+/api/posts endpoint
+- [ ] POST
+- [ ] GET
+- [ ] DELETE
+
+/admin/reset endpoint
+
+Define Postgres database in Goose
+SQLC for interaction with the database
+
+**Frontend**
+Header
+- [ ] Site Name
+
+New Post
+- [ ] Somewhere to enter new posts
+- HTMX to call POST /api/posts
+
+Posts
+- [ ] Table of posts
+- [ ] Delete button for each post
+- [ ] Button call DELETE /api/posts
+- [ ] Like button for each post?
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Using enviroment variables.")
+	} else {
+		fmt.Println("Loaded .env file.")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		MainPage().Render(r.Context(), w)
+	})
+
+	server := &http.Server{
+		Addr:         ":" + port,
+		Handler:      mux,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	log.Printf("Starting another-brick on port: %s\n", port)
+	log.Fatal(server.ListenAndServe())
+}
